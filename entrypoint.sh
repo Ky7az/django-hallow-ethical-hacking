46957de0e26656b9fab5dd9725857c6bb9c082e7
Ky7az <8583937+Ky7az@users.noreply.github.com>
2022-05-02 21:02:32 +0200

    HallowPentest Fixtures

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata phase port service tool vulnerability
 python manage.py collectstatic --no-input --clear
 
 exec "$@"
